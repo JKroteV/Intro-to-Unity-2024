920d428df13cc891dc2703f091ee99d997491305
J.J Ramos <Mrtortoise910@gmail.com>
2024-05-04 15:22:07 -0700

    Adding camera rotation, prefabs, and bullets spawning

--- My project/Assets/Scripts/move.cs
+++ My project/Assets/Scripts/move.cs
@@ -6,16 +6,24 @@ public class move : MonoBehaviour
 {
     public float speed = 5f; 
     public Vector3 jumpForce = new Vector3 (0.0f, 10.0f, 0.0f);
+    public float rotationSpeed = 1f; 
     private Rigidbody rb; 
+    private float yRotate = 0f; 
+    private Vector3 startPosition = Vector3.zero;
+    
     // Start is called before the first frame update
+
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        startPosition = transform.position;
     }
 
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
         { 
             
             rb.AddForce(jumpForce, ForceMode.Impulse); 
@@ -24,11 +32,28 @@ public class move : MonoBehaviour
     }
 
     // Update is called once per frame
+void RotateView()
+{
+yRotate += Input.GetAxis("Mouse X") * rotationSpeed;
+
+transform.eulerAngles = new Vector3(0, yRotate, 0);
+}
+
+bool isGrounded()
+{
+   int layerMask = LayerMask.GetMask("Ground");
+    Vector3 offset = new Vector3(0.0f, -1f, 0.0f);
+    return Physics.Raycast(transform.position + offset, Vector3.down,
+    0.1f,layerMask);
+
+}
+
+
     void Update()
     {
         Vector3 moveDir = Vector3.zero;  
         if (Input.GetKey(KeyCode.A))
-        {
+        {   
            moveDir.x = -1.0f;
         }
         if (Input.GetKey(KeyCode.W))
@@ -48,14 +73,26 @@ public class move : MonoBehaviour
         transform.right * moveDir.x;
         moveDir = movement * speed * Time.deltaTime;
 
-        transform.Translate(moveDir);
+        transform.Translate(moveDir, Space.World);
         
         Jump();
-
+        RotateView();
         // Vector3 position = transform.position;
         // position.x += speed * Time.deltaTime;
         // transform.position = position;
 
 
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    transform.position = startPosition;
+    rb.velocity = Vector3.zero;
+
+    }
+
+
+
 }
+
