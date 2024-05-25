f9b4d592837c705378192d39d7771fc4f665ca60
J.J Ramos <Mrtortoise910@gmail.com>
2024-05-25 15:14:21 -0700

    2D Player Movement, Camera Movement

--- Meow Unity/Assets/Scripts/Bullet.cs
+++ Meow Unity/Assets/Scripts/Bullet.cs
@@ -5,21 +5,18 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float lifeTime = 1f;
-
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, lifeTime);
+      Destroy(gameObject, lifeTime);  
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
-        {
-            Destroy(gameObject);
-        }
-    }
-
+if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        Destroy(gameObject);
+    
+    }   
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
